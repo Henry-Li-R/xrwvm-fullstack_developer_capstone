35126e780c7d554c9f406888749191dbfe197542
Henry-Li-R <runheng29.li@gmail.com>
2025-08-16 13:36:24 -0400

    Linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -21,7 +21,11 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(
-        default=2023, validators=[MinValueValidator(2015), MaxValueValidator(2023)]
+        default=2023,
+        validators=[
+            MinValueValidator(2015),
+            MaxValueValidator(2023),
+        ]
     )
 
     def __str__(self):
